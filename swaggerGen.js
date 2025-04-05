376e30a44ec1a179b3281ae3bc4eaa29b7382040
Mayank-Webseeder <joshi.mayank@webseeder.in>
2025-04-05 10:42:51 +0530

    updated api docs

--- swaggerGen.js
+++ swaggerGen.js
@@ -14,6 +14,7 @@ const doc = {
         { name: "Subcategories", description: "Subcategory management" },
         { name: "Home Page", description: "Homepage management" },
         { name: "Pages", description: "Page content management" },
+        { name: "Offers", description: "Offer section management" },
     ],
 };
 
@@ -39,19 +40,26 @@ swaggerAutogen(outputFile, endpointsFiles, doc).then(({ data }) => {
     Object.keys(updatedPaths).forEach((route) => {
         const pathObj = updatedPaths[route];
 
-        Object.keys(pathObj).forEach((method) => {
-            if (route.includes("/api/auth")) {
-                pathObj[method].tags = ["Auth"];
-            } else if (route.includes("/api/products")) {
-                pathObj[method].tags = ["Products"];
-            } else if (route.includes("/api/subcategories")) {
-                pathObj[method].tags = ["Subcategories"];
-            } else if (route.includes("/api/homepage")) {
-                pathObj[method].tags = ["Home Page"];
-            } else if (route.includes("/api/pages")) {
-                pathObj[method].tags = ["Pages"];
-            }
+        Object.keys(updatedPaths).forEach((route) => {
+            const pathObj = updatedPaths[route];
+
+            Object.keys(pathObj).forEach((method) => {
+                if (route.includes("/api/auth")) {
+                    pathObj[method].tags = ["Auth"];
+                } else if (route.includes("/api/products")) {
+                    pathObj[method].tags = ["Products"];
+                } else if (route.includes("/api/subcategories")) {
+                    pathObj[method].tags = ["Subcategories"];
+                } else if (route.includes("/api/homepage")) {
+                    pathObj[method].tags = ["Home Page"];
+                } else if (route.includes("/api/pages")) {
+                    pathObj[method].tags = ["Pages"];
+                } else if (route.includes("/api/offer-sections")) {
+                    pathObj[method].tags = ["Offers"];
+                }
+            });
         });
+
     });
 
     data.paths = updatedPaths;
